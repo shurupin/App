e3defc8aa05999539ed45abd7c3a807aebe7d71d
Stanislav Shurupin <stanislav,shurupin@gmail.com>
2020-03-29 01:04:47 +0300

    Added react-admin (first try)

--- App/ClientApp/src/index.tsx
+++ App/ClientApp/src/index.tsx
@@ -1,27 +1,38 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import 'proxy-polyfill';
+
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
-import { createBrowserHistory } from 'history';
-import configureStore from './store/configureStore';
+// import { Provider } from 'react-redux';
+// import { ConnectedRouter } from 'connected-react-router';
+// import { createBrowserHistory } from 'history';
+// import configureStore from './store/configureStore';
+
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import 'proxy-polyfill';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 // Create browser history to use in the Redux store
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') as string;
-const history = createBrowserHistory({ basename: baseUrl });
+//const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') as string;
+//const history = createBrowserHistory({ basename: baseUrl });
 
 // Get the application-wide store instance, prepopulating with state from the server where available.
-const store = configureStore(history);
+//const store = configureStore(history);
 
-ReactDOM.render(
+/* ReactDOM.render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
             <App />
         </ConnectedRouter>
     </Provider>,
-    document.getElementById('root'));
+    document.getElementById('root')); */
+ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
